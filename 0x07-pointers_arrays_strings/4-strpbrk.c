fd2fe497a5518e5d40e68328ec7b0530cb99aef1
kutiote30 <k.bando@alustudent.com>
2022-09-26 13:02:55 -0700

    4

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- *_strpbrk - The _strpbrk() function locates the first
+ * _strpbrk - The _strpbrk() function locates the first
  *  occurrence in the string s of any of the bytes in
  * the string accept
  * 
